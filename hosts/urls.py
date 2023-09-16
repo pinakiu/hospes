9949ae492a760e2aa7fe36089473adae20ee0e8a
Pinaki Upadhyay <pinaki.u.15@gmail.com>
2023-09-16 08:46:02 -0400

    Upload another simpler version of REST to streamline coding process.

--- hosts/urls.py
+++ hosts/urls.py
@@ -1,10 +1,12 @@
-from rest_framework.routers import DefaultRouter
-from .views import ConfigViewSet
+# from rest_framework.routers import DefaultRouter
+from django.contrib import admin
+from hosts import views 
 from django.urls import path, include
 
-router = DefaultRouter()
-router.register(r'config', ConfigViewSet)
+# router = DefaultRouter()
+# router.register(r'config', ConfigViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
+    path('getConfig/', views.getConfig),
 ]
--- hosts/views.py
+++ hosts/views.py
@@ -1,10 +1,18 @@
 # from django.http.response import Http404
-from rest_framework import viewsets 
+# from rest_framework import viewsets 
+from django.http import JsonResponse
 from .models import Config
 from .serializers import ConfigSerializer
 
-class ConfigViewSet(viewsets.ModelViewSet):
-    queryset = Config.objects.all()
-    serializer_class = ConfigSerializer
+def getConfig(request):
+    config = Config.objects.all()
+    serializer = ConfigSerializer(config, many=True)
+    # return JsonResponse(serializer.data, safe=False)
+    return JsonResponse({"drinks": serializer.data})
+    #returns {"drinks": [{id: key:}, {id:, key: }]}
+
+
+    # queryset = Config.objects.all()
+    # serializer_class = ConfigSerializer
 
 
