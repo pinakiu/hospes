48a7e3b99d01f667b41a8904a91dcc2a7765145a
Cole Smith <colesmith5400@gmail.com>
2023-09-16 08:17:54 -0400

    Add guest-host toggle

---
+++ hospes-frontend/src/components/ToggleButton.jsx
@@ -0,0 +1,64 @@
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import React, { useState } from 'react';
+
+const ToggleButton = () => {
+  const [selected, setSelected] = useState('Guest');
+
+  const handleSelect = (e, option) => {
+    e.preventDefault()
+    setSelected(option);
+  };
+
+  return (
+    <div className="d-inline-block border rounded">
+      <div className="position-relative" style={{ width: '200px', height: '40px' }}>
+        <div
+          className="position-absolute"
+          style={{
+            left: selected === 'Guest' ? '0' : '100px',
+            top: 0,
+            width: '100px',
+            height: '40px',
+            backgroundColor: '#ddd',
+            transition: 'left 0.3s ease-in-out',
+            borderRadius: '0.25rem',
+            textAlign: 'center',
+            lineHeight: '40px'
+          }}
+        >
+          &nbsp;
+        </div>
+        <button
+          onClick={(e) => handleSelect(e, 'Guest')}
+          className="position-absolute btn btn-link"
+          style={{
+            left: '0',
+            top: 0,
+            width: '100px',
+            height: '40px',
+            backgroundColor: 'transparent',
+            boxShadow: 'none'
+          }}
+        >
+          Guest
+        </button>
+        <button
+          onClick={(e) => handleSelect(e,'Host')}
+          className="position-absolute btn btn-link"
+          style={{
+            left: '100px',
+            top: 0,
+            width: '100px',
+            height: '40px',
+            backgroundColor: 'transparent',
+            boxShadow: 'none'
+          }}
+        >
+          Host
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ToggleButton;
