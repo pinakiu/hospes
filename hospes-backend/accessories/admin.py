4049bba433f9835ee72dab653b1a3beb74960057
Pinaki Upadhyay <pinaki.u.15@gmail.com>
2023-09-16 13:54:28 -0400

    Populated config

--- hospes-backend/accessories/admin.py
+++ hospes-backend/accessories/admin.py
@@ -1,6 +1,6 @@
-# from django.contrib import admin
-# from .models import Config, ConfigSet
+from django.contrib import admin
+from .models import Config, ConfigSet
 
-# models_list = [Config, ConfigSet]
-# admin.site.register(models_list)
-# # Register your models here.
+models_list = [Config, ConfigSet]
+admin.site.register(models_list)
+# Register your models here.
