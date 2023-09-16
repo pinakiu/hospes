e0bdacafb42ebd79b4fd080a267a59c5fd2546fb
Mahri <mahri.almaz@gmail.com>
2023-09-16 14:16:18 -0400

    add user authentication

--- hospes-backend/accessories/models.py
+++ hospes-backend/accessories/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-# from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 
 media_root = settings.MEDIA_ROOT
@@ -11,7 +11,6 @@ class Config(models.Model):
     def __str__(self):
         return self.config_name
 
-
 class ConfigSet(models.Model):
     config_set_id = models.AutoField(primary_key=True)
     config_id = models.ForeignKey(Config, on_delete=models.CASCADE)
@@ -19,20 +18,17 @@ class ConfigSet(models.Model):
 
     def __str__(self):
         return f"{self.config_id.config_name} {self.config_value}"
-
-# class CustomUser(AbstractUser):
-#     # first_name = models.CharField(max_length=200)
-#     # last_name = models.CharField(max_length=200)
-#     # username = models.CharField(max_length=200)
-#     # password = models.CharField(max_length=200)
-#     USER_TYPES = (
-#         ('guest', 'Guest'),
-#         ('host', 'Host'),  
-#     )
-#     user_type = models.CharField(max_length=10, choices=USER_TYPES, null = True, blank = True)
     
-#     def __str__(self):
-#         return self.username
+class User(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+
+    user_type = models.IntegerField(null = True, blank = True)
+
+    def __str__(self):
+        return self.username
 
 def user_directory_path(instance, filename):
     return 'BASE_DIR/media/{0}/{1}'.format(filename)
--- hospes-backend/accessories/serializers.py
+++ hospes-backend/accessories/serializers.py
@@ -1,5 +1,22 @@
 from rest_framework import serializers
-from .models import Config
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+        def create(self, validated_data):
+            user = User(
+                first_name = validated_data['first_name'],
+                last_name = validated_data['last_name'],
+                username=validated_data['username']
+            )
+            user.set_password(validated_data['password'])
+            user.save()
+            return user
+
 
 class ConfigSerializer(serializers.ModelSerializer):
     class Meta:
