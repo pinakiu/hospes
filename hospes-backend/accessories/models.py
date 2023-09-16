ddac9f02ce07ed7dad74c820c491effcfb8e6fed
Pinaki Upadhyay <pinaki.u.15@gmail.com>
2023-09-16 15:20:37 -0400

    Fixing some more things

--- hospes-backend/accessories/models.py
+++ hospes-backend/accessories/models.py
@@ -2,8 +2,6 @@ from django.db import models
 # from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 
-media_root = settings.MEDIA_ROOT
-
 class Config(models.Model):
     config_id = models.AutoField(primary_key=True)
     config_name = models.CharField(max_length=5000, unique=True)
@@ -19,37 +17,3 @@ class ConfigSet(models.Model):
 
     def __str__(self):
         return f"{self.config_id.config_name} {self.config_value}"
-
-# class CustomUser(AbstractUser):
-#     # first_name = models.CharField(max_length=200)
-#     # last_name = models.CharField(max_length=200)
-#     # username = models.CharField(max_length=200)
-#     # password = models.CharField(max_length=200)
-#     USER_TYPES = (
-#         ('guest', 'Guest'),
-#         ('host', 'Host'),  
-#     )
-#     user_type = models.CharField(max_length=10, choices=USER_TYPES, null = True, blank = True)
-    
-#     def __str__(self):
-#         return self.username
-
-def user_directory_path(instance, filename):
-    return 'BASE_DIR/media/{0}/{1}'.format(filename)
-
-class Photos(models.Model):
-    USER_TYPES = (
-        ('guest', 'Guest'),
-        ('host', 'Host'),  
-    )
-    PHOTO_TYPES = (
-        ('profile', 'Profile'),
-        ('home', 'Home'),  
-    )
-    # user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    user_type = models.CharField(max_length=10, choices=USER_TYPES)
-    photo_type = models.CharField(max_length=20, choices=PHOTO_TYPES)
-    photo = models.ImageField(upload_to=user_directory_path)
-
-    # def __str__(self):
-    #     return f"{self.user.username}'s {self.photo_type} photo"
--- hospes-backend/guests/models.py
+++ hospes-backend/guests/models.py
@@ -1,16 +1,27 @@
 from django.db import models
+from PIL import Image
+from django.conf import settings
+
+media_root = settings.MEDIA_ROOT
 # from django.contrib.auth import get_user_model
 
 # User = get_user_model()
 
 class GuestProfile(models.Model):
     # guest_account = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(null=True, blank=True, upload_to="media_root/guests/")
+    property_picture = models.ImageField(null=True, blank=True, upload_to="media_root/guests/")
+
+    age = models.IntegerField(null=True, blank=True)
+    gender = models.IntegerField(null=True, blank=True)
     location = models.IntegerField(null=True, blank=True)
     origin_country = models.IntegerField(null=True, blank=True)
     language = models.IntegerField(null=True, blank=True)
-    target_dates = models.JSONField(default=dict, null=True, blank=True)
-    target_price = models.FloatField(null=True, blank=True)
-    profile_photo = models.ImageField(max_length=50)
     dietary_restrictions = models.IntegerField(null=True, blank=True)
+
+    budget = models.FloatField(null=True, blank=True)
+    target_dates = models.JSONField(default=dict, null=True, blank=True)
+    num_guests = models.IntegerField(null=True, blank=True)
     description = models.CharField(max_length=2000)
+    rating = models.IntegerField(null=True, blank=True)
 
