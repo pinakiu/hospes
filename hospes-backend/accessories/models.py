52f244e928e52676820a3894bdf7a5aec957c7d8
Mahri <mahri.almaz@gmail.com>
2023-09-16 10:53:58 -0400

    move models to the corresponding apps

--- hospes-backend/accessories/models.py
+++ hospes-backend/accessories/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+# class Config(models.Model):
+#     config_id = models.AutoField(primary_key=True)
+#     config_name = models.CharField(max_length=500, unique=True)
+
+#     def __str__(self):
+#         return self.config_name
+    
+
+# class ConfigSet(models.Model):
+#     config_set_id = models.AutoField(primary_key=True)
+#     config_id = models.ForeignKey(Config, on_delete=models.CASCADE)
+#     config_value = models.CharField(max_length=500)
+
+#     def __str__(self):
+#         return self.config_value
+
+# class Account(models.Model):
+#     first_name = models.CharField(max_length=50)
+#     last_name = models.CharField(max_length=50)
+#     username = models.CharField(max_length=50)
+#     password = models.CharField(max_length=50)
+
+# class Photos(models.Model):
+#     host_profile = models.ForeignKey(HostProfile, on_delete=models.CASCADE)
+#     profile_photo = models.ImageField(max_length=50)
+#     home_photo = models.ImageField(max_length=50)
--- hospes-backend/hosts/models.py
+++ hospes-backend/hosts/models.py
@@ -1,26 +1,6 @@
 from django.db import models
 
-class Config(models.Model):
-    config_id = models.AutoField(primary_key=True)
-    config_name = models.CharField(max_length=500, unique=True)
 
-    def __str__(self):
-        return self.config_name
-    
-
-class ConfigSet(models.Model):
-    config_set_id = models.AutoField(primary_key=True)
-    config_id = models.ForeignKey(Config, on_delete=models.CASCADE)
-    config_value = models.CharField(max_length=500)
-
-    def __str__(self):
-        return self.config_value
-
-# class Account(models.Model):
-#     first_name = models.CharField(max_length=50)
-#     last_name = models.CharField(max_length=50)
-#     username = models.CharField(max_length=50)
-#     password = models.CharField(max_length=50)
 
 # class HostProfile(models.Model):
 #     host_account = models.OneToOneField(HostAccount, on_delete=models.CASCADE)
@@ -36,28 +16,10 @@ class ConfigSet(models.Model):
 #     description = models.CharField(max_length=50)
 
 
-# class GuestAccount(models.Model):
-#     first_name = models.CharField(max_length=50)
-#     last_name = models.CharField(max_length=50)
-#     username = models.CharField(max_length=50)
-#     password = models.CharField(max_length=50)
-
-# class GuestProfile(models.Model):
-#     guest_account = models.OneToOneField(GuestAccount, on_delete=models.CASCADE)
-#     location = models.IntegerField(null=True, blank=True)
-#     origin_country = models.IntegerField(null=True, blank=True)
-#     language = models.IntegerField(null=True, blank=True)
-#     dietary_restrictions = models.CharField(max_length=50)
-#     target_price = models.FloatField(null=True, blank=True)
-#     profile_photo = models.ImageField(max_length=50)
-#     target_dates = models.JSONField(default=dict, null=True, blank=True)
-#     description = models.CharField(max_length=50)
 
 
-# class Photos(models.Model):
-#     host_profile = models.ForeignKey(HostProfile, on_delete=models.CASCADE)
-#     profile_photo = models.ImageField(max_length=50)
-#     home_photo = models.ImageField(max_length=50)
+
+
 
 
 
